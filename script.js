32de8a6211f1f3a5f8585825cc022916ff9aee45
Alka Rani <1994r.alka@gmail.com>
2023-04-03 10:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your code here
+<div class="movie-container">
+  <img src="https://m.media-amazon.com/images/M/MV5BMTczMTM3NjYwOV5BMl5BanBnXkFtZTYwMTk1NTM5._V1_SX300.jpg" alt="Movie poster" class="moviePic">
+  <p class="movieDetail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel mauris quis tortor euismod interdum. Aenean nec nulla leo. Vivamus congue lobortis semper. Nulla facilisi. In hac habitasse platea dictumst. Suspendisse euismod, mauris ac hendrerit suscipit, lacus dui blandit urna, non sollicitudin velit velit ut lorem.</p>
+</div>
+
